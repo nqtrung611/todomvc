8fb546d9cdfb364b0a9228fc5f481c8a3165d238
DESKTOP-36E7S5O\QuangTrung <trungngoquang611@gmail.com>
2024-07-20 21:52:42 +0700

    update

--- script.js
+++ script.js
@@ -28,10 +28,12 @@ const displayTasks = () => {
     listTasks.innerHTML = "";
 
     //Fetch All The Keys in local storage
-    let tasks = Object.keys(localStorage);
-    tasks = tasks.sort(function(a, b){return a-b});
+    let listKeys = Object.keys(localStorage);
+    listKeys = listKeys.sort(function(a, b){return a-b});
 
-    for (let key of tasks) {
+
+    //Show tất cả các task đã được lưu trong localStorage
+    for (let key of listKeys) {
         //Lấy tất cả giá trị trong local storage
         let value = localStorage.getItem(key);
         let innerTask = document.createElement("li");
@@ -39,7 +41,7 @@ const displayTasks = () => {
         innerTask.setAttribute("id", key);
         innerTask.innerHTML = `
             <div class="view">
-                <img src="./images/unchecked.png" alt="">
+                <img alt="">
                 <label>${value.split("_")[1]}</label>
                 <button class="destroy"><i class="fa-solid fa-x"></i></button>
             </div>
@@ -74,11 +76,10 @@ const displayTasks = () => {
             //Xóa task ở local storage và xóa thẻ div hiển thị
             let parent = element.parentElement.parentElement;
             removeTask(parent.id);
-            // parent.remove();
-            // count -= 1;
         });
     });
 
+    //Giữ tab trạng thái select mỗi khi hàm displayTasks được gọi lại
     const checkbtn = document.querySelector("button.selected");
     tasks.forEach(function(todo) {
         switch(checkbtn.value) {
@@ -101,24 +102,33 @@ const displayTasks = () => {
                 break;
         }
     });
-    
-    tasks.forEach((element) => {
-        element.addEventListener('mouseover', function() {
-            element.querySelector(".destroy").style.display = "inline-block";
+
+    //Hover ẩn hiện nút xóa
+    tasks.forEach((task) => {
+        const btnDestroy = task.querySelector(".destroy");
+        task.addEventListener('mouseover', function() {
+            if (btnDestroy) {
+                btnDestroy.style.display = "inline-block";
+            }
         });
-        element.addEventListener('mouseleave', function() {
-            element.querySelector(".destroy").style.display = "none";
+        task.addEventListener('mouseleave', function() {
+            if (btnDestroy) {
+                btnDestroy.style.display = "none";
+            }
         });
     });
-    
+
+    //Ẩn hiện footer
     if (!tasks.length) {
         document.querySelector(".footer").style.display = "none";
     }
-    
+
+    //Hiện số task chưa hoàn thành
     const itemsLeft = document.querySelector(".todo-count strong");
     const itemsCompleted = document.querySelectorAll(".task.completed");
     itemsLeft.innerText = `${tasks.length - itemsCompleted.length}`;
-    
+
+    //Ẩn hiện nút Clear Completed
     const clearBtn = document.querySelector(".clear-completed");
     if (itemsCompleted.length) {
         clearBtn.style.display = "inline-block";
@@ -126,6 +136,7 @@ const displayTasks = () => {
         clearBtn.style.display = "none";
     }
 
+    //Click "Clear Completed" để xóa các task đã hoàn thành
     clearBtn.addEventListener('click', function() {
         let max = Object.keys(localStorage);
         for (let key of max) {
@@ -143,85 +154,75 @@ const displayTasks = () => {
         // }
     });
 
-    //Select All
-    // const btnSelected = document.querySelector(".selected");
-    // if (itemsLeft.textContent === '0') {
-    //     btnSelectAll.parentElement.lastElementChild.classList.add("selected");
-    // } else {
-    //     btnSelectAll.parentElement.lastElementChild.classList.remove("selected");
-    // }
-    // if (!tasks.length) {
-    //     btnSelectAll.parentElement.lastElementChild.style.display = "none";
-    // } else {
-    //     btnSelectAll.parentElement.lastElementChild.style.display = "flex";
-    // }
-    // btnSelectAll.addEventListener('click', function(e) {
-    //     e.preventDefault();
-    //     if (itemsCompleted.length === tasks.length) {
-    //         tasks.forEach(function(task) {
-    //             task.classList.remove("completed");
-    //             localStorage.setItem(`${task.id}`, `false_${task.innerText}`);
-    //             // updateStorage(task.id, task.innerText, false);
-    //         });
-    //         loadAgain();
-    //     } else {
-    //         tasks.forEach(function(task) {
-    //             if (!task.classList.contains("completed")) {
-    //                 task.classList.add("completed");
-    //                 localStorage.setItem(`${task.id}`, `true_${task.innerText}`);
-    //                 // updateStorage(task.id, task.innerText, true);
-    //             }
-    //         });
-    //         loadAgain();
-    //     }
-    // });
+    //Ẩn, hiện, in đậm nút Select All
+    if (tasks.length === 0) {
+        btnSelectAll.parentElement.lastElementChild.style.display = "none";
+    } else {
+        switch(checkbtn.value) {
+            case "all":
+                btnSelectAll.parentElement.lastElementChild.style.display = "flex";
+                break;
+            case "completed":
+                if (itemsCompleted.length === 0) {
+                    btnSelectAll.parentElement.lastElementChild.style.display = "none";
+                } else {
+                    btnSelectAll.parentElement.lastElementChild.style.display = "flex";
+                }
+                break;
+            case "incomplete":
+                if (itemsLeft.innerText === '0') {
+                    btnSelectAll.parentElement.lastElementChild.style.display = "none";
+                } else {
+                    btnSelectAll.parentElement.lastElementChild.style.display = "flex";
+                }
+                break;
+        }
+        if (itemsLeft.innerText === '0') {
+            btnSelectAll.parentElement.lastElementChild.classList.add("active");
+        } else {
+            btnSelectAll.parentElement.lastElementChild.classList.remove("active");
+        }
+    }
 
-    
-
-    //Edit Task 
-    // console.log(tasks);
+    //Edit Task
     tasks.forEach(function(task) {
         task.addEventListener('dblclick', function(e) {
-            if (!(e.target.tagName === "IMG")) {
-                // console.log(task.innerText);
-                const textElement = task.querySelector("label");
-                const textEdit = textElement.textContent;
-                task.querySelector(".destroy").remove();
-                task.querySelector("img").remove();
-                const inputElement = document.createElement('input');
-                inputElement.type = 'text';
-                inputElement.setAttribute('class', 'editing');
-                inputElement.value = textElement.textContent;
-                textElement.parentNode.replaceChild(inputElement, textElement);
-                inputElement.focus();
-                inputElement.addEventListener('keydown', function(event) {
-                    if (event.key === 'Escape') {
-                        updateStorage(task.id, textEdit, false);
-                    }
-                });
-                inputElement.addEventListener('keydown', function(event) {
-                    if (event.key === 'Enter') {
-                        if (inputElement.value.trim() === '') {
-                            alert("Vui lòng nhập công việc");
-                            updateStorage(task.id, textEdit, false);
-                        } else {
-                            updateStorage(task.id, inputElement.value.trim(), false);
-                        }
-                        console.log("enter");
-                    }
-                });
-                inputElement.addEventListener('blur', function() {
-                    console.log("blur");
-                    updateStorage(task.id, textEdit, false);
-                });
+            const textElement = task.querySelector("label");
+            const textEdit = textElement.textContent;
+            task.querySelector(".destroy").remove();
+            task.querySelector("img").remove();
+            const inputElement = document.createElement('input');
+            inputElement.type = 'text';
+            inputElement.setAttribute('class', 'editing');
+            inputElement.value = textElement.textContent;
+            textElement.parentNode.replaceChild(inputElement, textElement);
+            inputElement.focus();
+            let taskStatus = false;
+            if (task.classList.contains("completed")) {
+                taskStatus = true;
             }
+            inputElement.addEventListener('keydown', function(event) {
+                if (event.key === 'Escape') {
+                    updateStorage(task.id, textEdit, taskStatus);
+                }
+            });
+            inputElement.addEventListener('keydown', function(event) {
+                if (event.key === 'Enter') {
+                    if (inputElement.value.trim() === '') {
+                        alert("Vui lòng nhập công việc");
+                        updateStorage(task.id, textEdit, taskStatus);
+                    } else {
+                        updateStorage(task.id, inputElement.value.trim(), taskStatus);
+                    }
+                }
+            });
+            inputElement.addEventListener('blur', function() {
+                inputElement.remove();
+                displayTasks();
+            });
         });
     });
 };
-
-// const loadAgain = () => {
-//     displayTasks();
-// }
 
 //Xóa task ở local storage
 const removeTask = (taskValue) => {
@@ -248,11 +249,11 @@ document.querySelector("#push").addEventListener("click", () => {
 
 //Button select task (Tất cả, hoàn thành, chưa hoàn thành)
 const filterOption = document.querySelector(".filters");
-filterOption.addEventListener("click", filterTodo);
+filterOption.addEventListener('click', filterTodo);
 const btnOptions = document.querySelectorAll(".btn-option");
 
 function filterTodo(e) {
-    e.preventDefault();
+    // e.preventDefault();
     const todos = listTasks.childNodes;
     todos.forEach(function(todo) {
         switch(e.target.value) {
@@ -289,3 +290,23 @@ function filterTodo(e) {
     });
     displayTasks();
 }
+
+//Click Select All
+btnSelectAll.addEventListener('click', function() {
+    const itemsLeft = document.querySelector(".todo-count strong");
+    const todos = listTasks.childNodes;
+    if (itemsLeft.innerText === '0') {
+        todos.forEach(function(todo) {
+            todo.classList.remove("completed");
+            localStorage.setItem(`${todo.id}`, `false_${todo.innerText}`);
+        });
+    } else {
+        todos.forEach(function(todo) {
+            if (!todo.classList.contains("completed")) {
+                todo.classList.add("completed");
+                localStorage.setItem(`${todo.id}`, `true_${todo.innerText}`);
+            }
+        });
+    }
+    displayTasks();
+});
